53609949eaca720e134e992cf0c7b0f37e02028a
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-18 03:33:46 +0900

    M

--- app/Console/Commands/CreateYoutubePost.php
+++ app/Console/Commands/CreateYoutubePost.php
@@ -31,7 +31,7 @@ class CreateYoutubePost extends Command
      */
     public function __construct()
     {
-        parent::__construct();
+      parent::__construct();
     }
 
     /**
@@ -46,9 +46,9 @@ class CreateYoutubePost extends Command
 
      /* ない場合は強制終了 */
      if($word){
-        $word->fill(['done' => 1])->save();
-     }else{
-       return false;
+      $word->fill(['done' => 1])->save();
+    }else{
+     return false;
    }
    $title = $word->text;
    $author = "";
@@ -67,46 +67,50 @@ class CreateYoutubePost extends Command
     'q' => $title,
     'maxResults' => 10,
     'order' => 'viewCount'
-));
+  ));
 
    $ret_data = "";
 
-   foreach($searchResponse['items'] as $index => $item){
+   if(count($searchResponse['items']) < 3){
+    return false;
+  }
+
+  foreach($searchResponse['items'] as $index => $item){
     try{
-        $movie_code = $item['id']['videoId'];
-        $movie_title = $item['snippet']['title'];
-        $movie_description = $item['snippet']['description'];
-        $movie_author[] = $item['snippet']['channelTitle'];
-        if($index < 3){
-            $title_author[] = $item['snippet']['channelTitle'];
-        }
-        $movie_url = $baseurl.$movie_code;
-        $rank = $index + 1;
-        $ret_data .= "<h2>【{$rank}位】{$movie_title}</h2>" . PHP_EOL. PHP_EOL;
-        $ret_data .= '<iframe width="728" height="410" src="https://www.youtube.com/embed/'.$movie_code.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>' . PHP_EOL. PHP_EOL;
-        $ret_data .= "<blockquote>{$movie_description}</blockquote>" . PHP_EOL. PHP_EOL;
+      $movie_code = $item['id']['videoId'];
+      $movie_title = $item['snippet']['title'];
+      $movie_description = $item['snippet']['description'];
+      $movie_author[] = $item['snippet']['channelTitle'];
+      if($index < 3){
+        $title_author[] = $item['snippet']['channelTitle'];
+      }
+      $movie_url = $baseurl.$movie_code;
+      $rank = $index + 1;
+      $ret_data .= "<h2>【{$rank}位】{$movie_title}</h2>" . PHP_EOL. PHP_EOL;
+      $ret_data .= '<iframe width="728" height="410" src="https://www.youtube.com/embed/'.$movie_code.'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>' . PHP_EOL. PHP_EOL;
+      $ret_data .= "<blockquote>{$movie_description}</blockquote>" . PHP_EOL. PHP_EOL;
 
     }catch(Exception $e){
             // echo $e->getMessage();
     }
-}
+  }
 
-$author = implode('、',$movie_author);
-$title_author = implode('、',$title_author);
+  $author = implode('、',$movie_author);
+  $title_author = implode('、',$title_author);
 
-$post_description = "今、{$title}というワードが人気で検索回数が急上昇中です。そこで、今回は{$title}に関する人気のYoutube動画をランキング形式でまとめてみました！" . PHP_EOL . PHP_EOL;
-$post_description .= "{$author}の動画がランキング入りしています！" . PHP_EOL;
-$post_title = "今話題の{$title}のYoutube動画TOP10を紹介！{$title_author}など";
+  $post_description = "今、{$title}というワードが人気で検索回数が急上昇中です。そこで、今回は{$title}に関する人気のYoutube動画をランキング形式でまとめてみました！" . PHP_EOL . PHP_EOL;
+  $post_description .= "{$author}の動画がランキング入りしています！" . PHP_EOL;
+  $post_title = "今話題の{$title}のYoutube動画TOP10を紹介！{$title_author}など";
 
-$content = $post_description.$ret_data; 
-$mail = [
+  $content = $post_description.$ret_data; 
+  $mail = [
     'post_title' => $post_title,
     'content' => $content
-];
-Mail::send('emails.welcome', array('content' => $content), function($message) use ($post_title,$content){
+  ];
+  Mail::send('emails.welcome', array('content' => $content), function($message) use ($post_title,$content){
     $message->to('knowrop1208.ymatome@gmail.com')
     ->subject($post_title);
-});
+  });
 
 }
 }
